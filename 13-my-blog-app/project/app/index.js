d502d3dc52032718158f74bdbd18568bc7afa05e
Carlos Roberto <carlosmedeiroslima@gmail.com>
2021-07-28 19:43:39 -0300

    Creating my blog structure

---
+++ 13-my-blog-app/project/app/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var jquery = require('jquery');
+var angular = require('angular');
+
+require('angular-toastr');
+require('@uirouter/angularjs');
+
+require('./app');
+
+//controllers
+require('./view/blog-post-controller');
